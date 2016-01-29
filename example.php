a74978ce0e13c75fe835e3c03e462e0510154139
rafageist <ancorasoft@gmail.com>
2016-01-29 09:26:14 -0500

    update

--- example.php
+++ example.php
@@ -10,12 +10,12 @@ include "div.php";
 // Custom function
 function t($word)
 {
-    $dict = array('ejemplo basico' => 'basic example');
+	$dict = array('ejemplo basico' => 'basic example');
 
-    if (isset($dict[strtolower($word)]))
-        return $dict[strtolower($word)];
+	if (isset($dict[strtolower($word)]))
+		return $dict[strtolower($word)];
 
-    return $word;
+	return $word;
 }
 
 div::setAllowedFunction("t");
@@ -23,17 +23,13 @@ div::setAllowedFunction("t");
 // Custom parser/subparser
 function a($href)
 {
-    return '<a href = "' . $href . '">' . $href . '</a>';
+	return '<a href = "' . $href . '">' . $href . '</a>';
 }
 
 div::setSubParser("a");
 
 // Output
 echo new div("example.tpl", array(
-    "title" => "Ejemplo basico",
-    "year" => "2015",
-    "links" => array(
-        "Components" => "http://github.com/rrodriguezr/div-components",
-        "Tools" => "http://github.com/rrodriguezr/div-tools",
-        "Dialects" => "http://github.com/rrodriguezr/div-dialects",
-        "3rd Party Integrations" => "http://github.com/rrodriguezr/div-3rd-party")));
+	"title" => "Ejemplo basico",
+	"year" => "2015",
+	"links" => array("Core" => "http://github.com/rafageist/div", "Extras" => "http://github.com/rafageist/div-extras")));
