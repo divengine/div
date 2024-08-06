43da7e9f404686a4cb721cf6b84f1b82c7222ed7
rafageist <rafageist@hotmail.com>
2024-08-06 00:15:24 -0300

    more simple tests

--- tests/SimpleTest.php
+++ tests/SimpleTest.php
@@ -60,4 +60,78 @@ class SimpleTest extends TestCase
         $compiled = "$compiled";
         $this->assertEquals($expected, $compiled);
     }
+
+    public function testSimpleLoopWithObjectAndArray()
+    {
+        $tpl = 'Hello, [$names]{$name}!$_is_last  &  $_is_last![/$names]!';
+        $data = [
+            'names' => [
+                ['name' => 'John Doe'],
+                ['name' => 'Jane Doe']
+            ]
+        ];
+        $expected = 'Hello, John Doe & Jane Doe!';
+        $compiled = new div($tpl, $data);
+        $compiled = "$compiled";
+        $this->assertEquals($expected, $compiled);
+    }
+
+    public function testSimpleLoopWithArrayAndArray()
+    {
+        $tpl = 'Hello, [$names]{$name}!$_is_last  &  $_is_last![/$names]!';
+        $data = [
+            'names' => [
+                ['name' => 'John Doe'],
+                ['name' => 'Jane Doe']
+            ]
+        ];
+        $expected = 'Hello, John Doe & Jane Doe!';
+        $compiled = new div($tpl, $data);
+        $compiled = "$compiled";
+        $this->assertEquals($expected, $compiled);
+    }
+
+    public function testSimpleLoopWithArrayAndObject()
+    {
+        $tpl = 'Hello, [$names]{$name}!$_is_last  &  $_is_last![/$names]!';
+        $data = new stdClass();
+        $data->names = [
+            ['name' => 'John Doe'],
+            ['name' => 'Jane Doe']
+        ];
+        $expected = 'Hello, John Doe & Jane Doe!';
+        $compiled = new div($tpl, $data);
+        $compiled = "$compiled";
+        $this->assertEquals($expected, $compiled);
+    }
+
+    public function testSimpleFormula()
+    {
+        $tpl = '(# 1 + 2 #)';
+        $data = [];
+        $expected = '3';
+        $compiled = new div($tpl, $data);
+        $compiled = "$compiled";
+        $this->assertEquals($expected, $compiled);
+    }
+
+    public function testSimpleFormulaWithObject()
+    {
+        $tpl = '(# 1 + 2 #)';
+        $data = new stdClass();
+        $expected = '3';
+        $compiled = new div($tpl, $data);
+        $compiled = "$compiled";
+        $this->assertEquals($expected, $compiled);
+    }
+
+    public function testSimpleLoopOfFormula()
+    {
+        $tpl = 'Hello, [$names](# 1 + 2 #)!$_is_last  &  $_is_last![/$names]!';
+        $data = ['names' => ['John Doe', 'Jane Doe']];
+        $expected = 'Hello, 3 & 3!';
+        $compiled = new div($tpl, $data);
+        $compiled = "$compiled";
+        $this->assertEquals($expected, $compiled);
+    }
 }
